e710b50a7ed078a75e144d1994d616bf260eaa4b
lianghung523 <lianghung523@gmail.com>
2021-11-22 18:12:20 +0800

    db&entity change

--- src/main/java/com/origin/cafe/entity/Administrator.java
+++ src/main/java/com/origin/cafe/entity/Administrator.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="adminstrator")
+@Table(name="administrator")
 public class Administrator {
 	
 	//定義fields
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,166 +1,166 @@
 -- 建立資料庫
--- DROP DATABASE  IF EXISTS `orginal_cafe`;
--- CREATE DATABASE  IF NOT EXISTS `orginal_cafe`;
+-- drop database  if exists `orginal_cafe`;
+-- create database  if not exists `orginal_cafe`;
 
--- ===========MEMBER TABLE================
-DROP TABLE IF EXISTS `MEMBER`;
-CREATE TABLE MEMBER 
-(MEM_NO int(10) NOT NULL AUTO_INCREMENT COMMENT '會員編號', 
-	MEM_USERNAME varchar(255) NOT NULL comment '會員帳號',
-	MEM_PASSWORD varchar(255) NOT NULL COMMENT '會員密碼', 
-	MEM_NAME varchar(255) NOT NULL COMMENT '會員姓名', 
-	MEM_PHONE varchar(255) NOT NULL COMMENT '會員手機號碼',  
-	MEM_EMAIL varchar(255) NOT NULL COMMENT '會員電子信箱', 
-	MEM_ADDRESS varchar(255) NOT NULL COMMENT '會員地址', 
-	MEM_BIRTH date NOT NULL COMMENT '會員生日', 
-	MEM_IMG varchar(255) COMMENT '會員圖片', 
-	MEM_STATUS tinyint(3) NOT NULL COMMENT '會員狀態', 
-	CR_DATE datetime NOT NULL COMMENT '建立日期', 
-	MD_DATE datetime NOT NULL COMMENT '修改日期', 
-	PRIMARY KEY (MEM_NO));
+-- ===========member table================
+drop table if exists `member`;
+create table member 
+(mem_no int(10) not null auto_increment comment '會員編號', 
+	mem_username varchar(255) not null comment '會員帳號',
+	mem_password varchar(255) not null comment '會員密碼', 
+	mem_name varchar(255) not null comment '會員姓名', 
+	mem_phone varchar(255) not null comment '會員手機號碼',  
+	mem_email varchar(255) not null comment '會員電子信箱', 
+	mem_address varchar(255) not null comment '會員地址', 
+	mem_birth date not null comment '會員生日', 
+	mem_img varchar(255) comment '會員圖片', 
+	mem_status tinyint(3) not null comment '會員狀態', 
+	cr_date datetime not null comment '建立日期', 
+	md_date datetime not null comment '修改日期', 
+	primary key (mem_no));
 
-INSERT INTO `MEMBER`
-(`MEM_USERNAME`,`MEM_PASSWORD`,`MEM_NAME`,`MEM_PHONE`,`MEM_EMAIL`,`MEM_ADDRESS`,`MEM_BIRTH`,`MEM_IMG`,`MEM_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `member`
+(`mem_username`,`mem_password`,`mem_name`,`mem_phone`,`mem_email`,`mem_address`,`mem_birth`,`mem_img`,`mem_status`,`cr_date`,`md_date`) values 
 ('choco','8825252','黃淵和','0988877788','john323@gmail.com','台北市遼寧街22號','2000-01-01','img','1','2021-01-01','2021-01-01');
-INSERT INTO `MEMBER`
-(`MEM_USERNAME`,`MEM_PASSWORD`,`MEM_NAME`,`MEM_PHONE`,`MEM_EMAIL`,`MEM_ADDRESS`,`MEM_BIRTH`,`MEM_IMG`,`MEM_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `member`
+(`mem_username`,`mem_password`,`mem_name`,`mem_phone`,`mem_email`,`mem_address`,`mem_birth`,`mem_img`,`mem_status`,`cr_date`,`md_date`) values 
 ('lalao','2225252','黃錢和','0988877218','john123@gmail.com','台北市遼寧街21號','2000-01-01','img','1','2021-01-01','2021-01-01');
-INSERT INTO `MEMBER`
-(`MEM_USERNAME`,`MEM_PASSWORD`,`MEM_NAME`,`MEM_PHONE`,`MEM_EMAIL`,`MEM_ADDRESS`,`MEM_BIRTH`,`MEM_IMG`,`MEM_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `member`
+(`mem_username`,`mem_password`,`mem_name`,`mem_phone`,`mem_email`,`mem_address`,`mem_birth`,`mem_img`,`mem_status`,`cr_date`,`md_date`) values 
 ('babap','4445666','淵平和','0939877328','john567@gmail.com','台北市遼寧街23號','2000-01-01','img','1','2021-01-01','2021-01-01');
-INSERT INTO `MEMBER`
-(`MEM_USERNAME`,`MEM_PASSWORD`,`MEM_NAME`,`MEM_PHONE`,`MEM_EMAIL`,`MEM_ADDRESS`,`MEM_BIRTH`,`MEM_IMG`,`MEM_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `member`
+(`mem_username`,`mem_password`,`mem_name`,`mem_phone`,`mem_email`,`mem_address`,`mem_birth`,`mem_img`,`mem_status`,`cr_date`,`md_date`) values 
 ('bfbfq','2234234','蔣淵和','0988777788','john321@gmail.com','台北市遼寧街24號','2000-01-01','img','1','2021-01-01','2021-01-01');
-INSERT INTO `MEMBER`
-(`MEM_USERNAME`,`MEM_PASSWORD`,`MEM_NAME`,`MEM_PHONE`,`MEM_EMAIL`,`MEM_ADDRESS`,`MEM_BIRTH`,`MEM_IMG`,`MEM_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `member`
+(`mem_username`,`mem_password`,`mem_name`,`mem_phone`,`mem_email`,`mem_address`,`mem_birth`,`mem_img`,`mem_status`,`cr_date`,`md_date`) values 
 ('fgfgr','1234456','王和平','0988333788','john678@gmail.com','台北市遼寧街25號','2000-01-01','img','1','2021-01-01','2021-01-01');
 
 -- 會員狀態 0:未啟用 1:已啟用
 
--- ===========DISH TABLE================
-DROP TABLE IF EXISTS `DISH`;
-CREATE TABLE `DISH` 
-(DISH_NO int(10) NOT NULL AUTO_INCREMENT COMMENT '餐點編號', 
-	DISH_IMG varchar(255) COMMENT '餐點圖片', 
-	DISH_NAME varchar(255) NOT NULL COMMENT '餐點名稱', 
-	DISH_PRICE int(10) NOT NULL COMMENT '餐點價格', 
-	DISH_NOTE varchar(255) NOT NULL COMMENT '餐點信息', 
-	DISH_STATUS tinyint(3) COMMENT '餐點狀態', 
-	DISH_QUANTITY int(10) NOT NULL COMMENT '餐點數量', 
-	CR_DATE datetime NOT NULL COMMENT '創建日期', 
-	MD_DATE datetime NOT NULL COMMENT '修改日期', 
-	DISH_TYPE varchar(255) NOT NULL COMMENT '餐點種類', 
-	PRIMARY KEY (DISH_NO));
+-- ===========dish table================
+drop table if exists `dish`;
+create table `dish` 
+(dish_no int(10) not null auto_increment comment '餐點編號', 
+	dish_img varchar(255) comment '餐點圖片', 
+	dish_name varchar(255) not null comment '餐點名稱', 
+	dish_price int(10) not null comment '餐點價格', 
+	dish_note varchar(255) not null comment '餐點信息', 
+	dish_status tinyint(3) comment '餐點狀態', 
+	dish_quantity int(10) not null comment '餐點數量', 
+	cr_date datetime not null comment '創建日期', 
+	md_date datetime not null comment '修改日期', 
+	dish_type varchar(255) not null comment '餐點種類', 
+	primary key (dish_no));
 
 -- 餐點狀態 0:下架 1:上架
 
-INSERT INTO `DISH` 
-(`DISH_IMG`,`DISH_NAME`,`DISH_PRICE`,`DISH_NOTE`,`DISH_STATUS`,`DISH_QUANTITY`,`CR_DATE`,`MD_DATE`,`DISH_TYPE`) VALUES 
+insert into `dish` 
+(`dish_img`,`dish_name`,`dish_price`,`dish_note`,`dish_status`,`dish_quantity`,`cr_date`,`md_date`,`dish_type`) values 
 ('https://images.pexels.com/photos/9700895/pexels-photo-9700895.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','推薦菜單1','250','好吃的燉飯','1','100','1990-07-04','1990-07-04','推薦菜色');
 
--- ===========ORDER TABLE================
-DROP TABLE IF EXISTS `ORD`;
-CREATE TABLE `ORD` 
-(ORD_NO int(10) NOT NULL AUTO_INCREMENT COMMENT '訂單編號', 
-	MEM_NO int(10) NOT NULL COMMENT '會員編號', 
-	ORD_TIME datetime NOT NULL COMMENT '訂單時間', 
-	ORD_PRICE int(10) NOT NULL COMMENT '訂單總金額', 
-	ORD_STATUS tinyint(3) COMMENT '訂單狀態', 
-	CR_DATE datetime NOT NULL COMMENT '創建日期', 
-	MD_DATE datetime NOT NULL COMMENT '修改日期', 
-	PRIMARY KEY (ORD_NO));
+-- ===========order table================
+drop table if exists `ord`;
+create table `ord` 
+(ord_no int(10) not null auto_increment comment '訂單編號', 
+	mem_no int(10) not null comment '會員編號', 
+	ord_time datetime not null comment '訂單時間', 
+	ord_price int(10) not null comment '訂單總金額', 
+	ord_status tinyint(3) comment '訂單狀態', 
+	cr_date datetime not null comment '創建日期', 
+	md_date datetime not null comment '修改日期', 
+	primary key (ord_no));
 
-INSERT INTO `ORD`
-(`MEM_NO`,`ORD_TIME`,`ORD_PRICE`,`ORD_STATUS`,`CR_DATE`,`MD_DATE`) VALUES 
+insert into `ord`
+(`mem_no`,`ord_time`,`ord_price`,`ord_status`,`cr_date`,`md_date`) values 
 ('1','2021-01-01','300','1','2021-01-01','2021-01-01');
 
 -- 訂單狀態 0:未確認 1:已付款 2:已取餐  3:取消 
 
 
--- ===========CONTACT_US TABLE================
-DROP TABLE IF EXISTS `CONTACT_US`;
-CREATE TABLE CONTACT_US 
-(CON_NO int(10) NOT NULL AUTO_INCREMENT COMMENT '留言板編號', 
-	CON_NAME varchar(255) NOT NULL COMMENT '留言者', 
-	CON_EMAIL varchar(255) NOT NULL COMMENT '留言者email', 
-	CON_PHONE varchar(255) NOT NULL COMMENT '留言者電話', 
-	CON_MSG text NOT NULL COMMENT '留言者信息', 
-	CON_TIME datetime NOT NULL COMMENT '留言時間', 
-	PRIMARY KEY (CON_NO));
+-- ===========contact_us table================
+drop table if exists `contact_us`;
+create table contact_us 
+(con_no int(10) not null auto_increment comment '留言板編號', 
+	con_name varchar(255) not null comment '留言者', 
+	con_email varchar(255) not null comment '留言者email', 
+	con_phone varchar(255) not null comment '留言者電話', 
+	con_msg text not null comment '留言者信息', 
+	con_time datetime not null comment '留言時間', 
+	primary key (con_no));
 
-INSERT INTO `CONTACT_US`
-(`CON_NAME`,`CON_EMAIL`,`CON_PHONE`,`CON_MSG`,`CON_TIME`) VALUES 
+insert into `contact_us`
+(`con_name`,`con_email`,`con_phone`,`con_msg`,`con_time`) values 
 ('john','hj32@gmail.com','0928777654','人外有人天，天外有天，相愛容易，相處極端，認為總是它人錯在先，互不信，互乜不容，如此日子茫然啊！','2021-01-01');
 
 
 
 
--- ===========ORDER_LIST TABLE================
-DROP TABLE IF EXISTS `ORDER_LIST`;
-CREATE TABLE ORDER_LIST (ORD_NO int(10) NOT NULL COMMENT '訂單編號', 
-	DISH_NO int(10) NOT NULL COMMENT '餐點編號', 
-	ORD_QUANTITY int(10) NOT NULL COMMENT '訂單數量', 
-	ORD_PRICEEACH int(10) COMMENT '每件商品價格', 
-	PRIMARY KEY (ORD_NO, DISH_NO));
+-- ===========order_list table================
+drop table if exists `order_list`;
+create table order_list (ord_no int(10) not null comment '訂單編號', 
+	dish_no int(10) not null comment '餐點編號', 
+	ord_quantity int(10) not null comment '訂單數量', 
+	ord_priceeach int(10) comment '每件商品價格', 
+	primary key (ord_no, dish_no));
 
-INSERT INTO `ORDER_LIST`
-(`ORD_NO`,`DISH_NO`,`ORD_QUANTITY`,`ORD_PRICEEACH`) VALUES 
+insert into `order_list`
+(`ord_no`,`dish_no`,`ord_quantity`,`ord_priceeach`) values 
 ('1','1','1','300');
 
 
 
 
 
--- ===========ADMINISTRATOR TABLE================
-DROP TABLE IF EXISTS `ADMINISTRATOR`;
-CREATE TABLE ADMINISTRATOR 
-(ADM_NO int(10) NOT NULL AUTO_INCREMENT COMMENT '管理者編號', 
-	ADM_ACCOUNT varchar(255) NOT NULL COMMENT '管理者帳戶', 
-	ADM_PASWORD varchar(255) NOT NULL COMMENT '管理者密碼', 
-	ADM_NAME varchar(255) NOT NULL COMMENT '管理者姓名', 
-	ADM_ADDRESS varchar(255) NOT NULL COMMENT '管理者地址', 
-	ADM_PHONE varchar(255) NOT NULL COMMENT '管理者電話', 
-	ADM_LEVEL varchar(255) NOT NULL COMMENT '管理者階級', 
-	ADM_STATUS tinyint(3) NOT NULL COMMENT '管理者狀態', 
-	PRIMARY KEY (ADM_NO));
+-- ===========administrator table================
+drop table if exists `administrator`;
+create table administrator 
+(adm_no int(10) not null auto_increment comment '管理者編號', 
+	adm_account varchar(255) not null comment '管理者帳戶', 
+	adm_pasword varchar(255) not null comment '管理者密碼', 
+	adm_name varchar(255) not null comment '管理者姓名', 
+	adm_address varchar(255) not null comment '管理者地址', 
+	adm_phone varchar(255) not null comment '管理者電話', 
+	adm_level varchar(255) not null comment '管理者階級', 
+	adm_status tinyint(3) not null comment '管理者狀態', 
+	primary key (adm_no));
 
-INSERT INTO `ADMINISTRATOR`
-(`ADM_ACCOUNT`,`ADM_PASWORD`,`ADM_NAME`,`ADM_ADDRESS`,`ADM_PHONE`,`ADM_LEVEL`,`ADM_STATUS`) VALUES 
-('King','8785657','金','台北市富錦街20號','0908766678','最高管理者','1');
+insert into `administrator`
+(`adm_account`,`adm_pasword`,`adm_name`,`adm_address`,`adm_phone`,`adm_level`,`adm_status`) values 
+('king','8785657','金','台北市富錦街20號','0908766678','最高管理者','1');
 
 -- 管理者狀態 0:未啟用 1:已啟用
 
--- ===========ADM_FUNCTION TABLE================
-DROP TABLE IF EXISTS `ADM_FUNCTION`;
-CREATE TABLE ADM_FUNCTION 
-(FUNCTION_NO int(10) NOT NULL AUTO_INCREMENT, 
-	FUNCTION_DETAIL varchar(255) NOT NULL, 
-	PRIMARY KEY (FUNCTION_NO));
+-- ===========adm_function table================
+drop table if exists `adm_function`;
+create table adm_function 
+(function_no int(10) not null auto_increment, 
+	function_detail varchar(255) not null, 
+	primary key (function_no));
 
-INSERT INTO `ADM_FUNCTION`
-(`FUNCTION_DETAIL`) VALUES 
+insert into `adm_function`
+(`function_detail`) values 
 ('系統信件發送');
 
 
--- ===========ADM＿AUTHORITY TABLE================
-DROP TABLE IF EXISTS `ADM＿AUTHORITY`;
-CREATE TABLE `ADM＿AUTHORITY` 
-(ADM_NO int(10) NOT NULL COMMENT '管理者編號', 
-	FUNCTION_NO int(10) NOT NULL COMMENT '功能編號', 
-	PRIMARY KEY (ADM_NO, FUNCTION_NO));
+-- ===========adm＿authority table================
+drop table if exists `adm＿authority`;
+create table `adm＿authority` 
+(adm_no int(10) not null comment '管理者編號', 
+	function_no int(10) not null comment '功能編號', 
+	primary key (adm_no, function_no));
 
-INSERT INTO `ADM＿AUTHORITY`
-(`ADM_NO`,`FUNCTION_NO`) VALUES 
+insert into `adm＿authority`
+(`adm_no`,`function_no`) values 
 ('1','1');
 
 
 
 
-ALTER TABLE `ADM＿AUTHORITY` ADD CONSTRAINT `ADM＿AUTHORITY_ibfk_1` FOREIGN KEY (ADM_NO) REFERENCES ADMINISTRATOR (ADM_NO);
-ALTER TABLE `ADM＿AUTHORITY` ADD CONSTRAINT `ADM＿AUTHORITY_ibfk_2` FOREIGN KEY (FUNCTION_NO) REFERENCES ADM_FUNCTION (FUNCTION_NO);
-ALTER TABLE `ORD` ADD CONSTRAINT ORD_ibfk_1 FOREIGN KEY (MEM_NO) REFERENCES MEMBER (MEM_NO);
-ALTER TABLE ORDER_LIST ADD CONSTRAINT ORDER_LIST_ibfk_1 FOREIGN KEY (ORD_NO) REFERENCES `ORD` (ORD_NO);
-ALTER TABLE ORDER_LIST ADD CONSTRAINT ORDER_LIST_ibfk_2 FOREIGN KEY (DISH_NO) REFERENCES DISH (DISH_NO);
+alter table `adm＿authority` add constraint `adm＿authority_ibfk_1` foreign key (adm_no) references administrator (adm_no);
+alter table `adm＿authority` add constraint `adm＿authority_ibfk_2` foreign key (function_no) references adm_function (function_no);
+alter table `ord` add constraint ord_ibfk_1 foreign key (mem_no) references member (mem_no);
+alter table order_list add constraint order_list_ibfk_1 foreign key (ord_no) references `ord` (ord_no);
+alter table order_list add constraint order_list_ibfk_2 foreign key (dish_no) references dish (dish_no);
 
 
 
