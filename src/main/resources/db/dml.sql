3ece8c55197e31d7eff58f8145f357103cb3128f
chichi <j43343@gmail.com>
2021-11-23 17:24:45 +0800

    modify ermodel

--- src/main/resources/db/dml.sql
+++ src/main/resources/db/dml.sql
@@ -19,7 +19,7 @@ insert into `member`
 
 insert into `dish` 
 (`dish_img`,`dish_name`,`dish_price`,`dish_note`,`dish_status`,`dish_quantity`,`cr_date`,`md_date`,`dish_type`) values 
-('https://images.pexels.com/photos/9700895/pexels-photo-9700895.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','推薦菜單1','250','好吃的燉飯','1','100','1990-07-04','1990-07-04','推薦菜色');
+('https://images.pexels.com/photos/9700895/pexels-photo-9700895.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500','RECOMMEND','250','好吃的燉飯','1','100','1990-07-04','1990-07-04','推薦菜色');
 
 insert into `ord`
 (`mem_no`,`ord_time`,`ord_price`,`ord_status`,`cr_date`,`md_date`) values 
